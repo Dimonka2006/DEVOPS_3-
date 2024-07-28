9a189e23f0f4256562419113f65998bd827590a0
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-07-28 14:54:37 +0500

    #5

--- supper_obrabotchik.py
+++ supper_obrabotchik.py
@@ -4,14 +4,14 @@ import csv
 def save_csv_to_sqlite(csv_filepath, db_filepath):
     # Открытие файла CSV
     with open(csv_filepath, newline='') as csvfile:
-        reader = csv.reader(csvfile)
+        reader = csv.DictReader(csvfile)
         # Создание подключения к базе данных
         conn = sqlite3.connect(db_filepath)
         cur = conn.cursor()
         # Создаем таблицу для хранения данных
-        cur.execute('''CREATE TABLE IF NOT EXISTS date (
+        cur.execute('''CREATE TABLE IF NOT EXISTS users(
             name TEXT,
-            birth BLOB,
+            birth TEXT,
             job TEXT,
             company TEXT,
             country TEXT,
@@ -29,11 +29,33 @@ def save_csv_to_sqlite(csv_filepath, db_filepath):
         # email;card_number;card_expire;security_code;url
 
         # Проходим по каждой строке CSV файла и записываем данные в таблицу
-        next(reader)  # Пропускаем заголовок CSV файла
+        # next(reader)  # Пропускаем заголовок CSV файла
+        # for row in reader:
+        #     cur.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", tuple(row))
+        # # Проверяем количество добавленных строк
+        # if cur.rowcount != 0:
+        #     print(f"{cur.rowcount} rows inserted successfully.")
+        # else:
+        #     print("No rows were inserted.")
+        # # Сохраняем изменения в базе данных
+        # conn.commit()
+        # # Закрываем соединение с базой данных
+        # conn.close()
+        # # Не забываем закрыть курсор
+        # cur.close()
+
+#save_csv_to_sqlite('file.csv', 'db/database.db')
+        # Вставка данных из CSV в базу данных
         for row in reader:
-            cur.execute("INSERT INTO date VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+            values = []
+            for key in row:
+                values.append("'" + str(row[key]) + "'")
+            sql = "INSERT INTO users VALUES (" + ",".join(values) + ")"
+            cur.execute(sql)
         # Сохраняем изменения в базе данных
         conn.commit()
         # Закрываем соединение с базой данных
         conn.close()
+
+# Вызов функции для импорта данных
 save_csv_to_sqlite('file.csv', 'db/database.db')
