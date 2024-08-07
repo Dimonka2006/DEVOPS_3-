5abc1efa6730b419ab52e900f8113c0d1d3cd187
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-08-07 17:06:01 +0500

    popitka №5

--- supper_obrabotchik_4.py
+++ supper_obrabotchik_4.py
@@ -9,61 +9,57 @@ source_dir = 'archive'
 #print(os.listdir(source_dir))
 # Путь к базе данных
 db_path = 'database.db'
+sql_path = 'create_table.sql'
 
-# Создание подключения к базе данных
-conn = sqlite3.connect(db_path)
-cur = conn.cursor()
+
 # Создаем таблицу для хранения данных
 #def create_table():
-cur.execute('''CREATE TABLE IF NOT EXISTS users(
-    name VARCHAR(60),
-    birth VARCHAR(10),
-    job VARCHAR(60),
-    company VARCHAR(60),
-    country VARCHAR(30),
-    city VARCHAR(30),
-    address VARCHAR(60),
-    zip_code VARCHAR(5),                                         
-    phone VARCHAR(20),
-    email VARCHAR(60),
-    card_number VARCHAR(20),
-    card_expire VARCHAR(5),
-    security_code VARCHAR(4),
-    url VARCHAR(20)
-    )''')
+print(os.path.exists(sql_path))
+
+# with open(sql_path, 'r') as file, sqlite3.connect('database.db') as conn:
+#     #print(file.read())
+#     conn.executescript(file.read())
+
+def execute_sql_script(sql_path, db_path):
+    with open(sql_path, 'r') as file, sqlite3.connect(db_path) as conn:
+        conn.executescript(file.read())
 
 
+def unpack_gz():
+    #print(os.listdir(source_dir))
+    # Перебираем все файлы в папке
+    for file in os.listdir(source_dir):
+        # Распаковываем архив
+        with gzip.open(os.path.join(source_dir, file), 'rt') as gz:
+                #gz.extractall(path=source_dir)
+            reader = gz.readlines()
+            count = 0
+            cur = conn.cursor()
+            conn = sqlite3.connect('database.db')
 
-#def unpack_gz():
-#print(os.listdir(source_dir))
-# Перебираем все файлы в папке
-for file in os.listdir(source_dir):
-    # Распаковываем архив
-    with gzip.open(os.path.join(source_dir, file), 'rt') as gz:
-            #gz.extractall(path=source_dir)
-        reader = gz.readlines()
-        count = 0
+            for row in reader:  
+                #print(row)
+                if count > 0:
+                    if count %2 != 0:
+                        row1 = row
+                    else:
+                        row1 = row1 + row
+                        #print(row1)       
+                        st = ''.join(row1) 
+                        elem = st.split(';') # 
+                        print(elem)
+                        cur.execute("INSERT INTO country VALUES (2, elem[5]) ON DUPLICATE KEY UPDATE contry = elem[5]")
+                        cur.execute("INSERT INTO city VALUES (2, elem[6])ON DUPLICATE KEY UPDATE city = elem[6]")
+                        #cur.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", list(elem))
 
-        for row in reader:  
-            #print(row)
-            if count > 0:
-                if count %2 != 0:
-                    row1 = row
-                else:
-                    row1 = row1 + row
-                    #print(row1)       
-                    st = ''.join(row1) 
-                    elem = st.split(';') # 
-                    #print(elem)
-                    cur.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", list(elem))
-            count = count + 1 
+                count = count + 1 
 
         # Сохраняем изменения в базе данных
-conn.commit()
+    conn.commit()
         # закрываем курсор
-cur.close()
+    #cur.close()
         # Закрываем соединение с базой данных
-conn.close()
+    conn.close()
 
 # Вызов функции для импорта данных
 #save_csv_to_sqlite('file.csv', 'database.db')
